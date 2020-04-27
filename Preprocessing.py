f3d45ea450f83248b9793e576edc4f809ed8169c
CosmicPotato <54864515+msunde137@users.noreply.github.com>
2020-04-27 18:52:07 -0400

    Swipe data included

--- Preprocessing.py
+++ Preprocessing.py
@@ -12,7 +12,35 @@ from sklearn.neighbors import KNeighborsClassifier
 import pickle as pc
 
 
-def preprocess(users, segments):
+def swipe_preprocessor(data_path, path_type):
+    data_frame = pd.read_csv(data_path)
+
+    group = data_frame["UserID"]
+    agg = data_frame.groupby([group])
+    for user in agg:
+        # make new path if one doesn't already exist
+        user_swipes = []
+        group = user[1]["SwipeID"]
+        agg1 = user[1].groupby([group])
+        for swipe in agg1:
+            swipe[1].index = range(len(swipe[1].index))
+            user_swipes.append(swipe[1])
+
+        user_id = user[0]
+        path = "DataByUser\\User" + str(user_id)
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        # serialize all the data for each user
+        pickle_out = open(path + "\\" + path_type, "wb")
+        pc.dump(user_swipes, pickle_out)
+        pickle_out.close()
+
+        print(user_swipes)
+    pass
+
+
+def gait_preprocessor(users, segments):
     for user in users:
         print("Preprocessing User" + str(user))
         a_path = "BB-MAS_Dataset/" + str(user) + "/" + str(user) + "_PocketPhone_Accelerometer_(Samsung_S6).csv"
